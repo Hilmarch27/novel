8764eb9cc7fe01b10b3e5fd8b8b67ca05b7c65f7
Hilman <117459721+Hilmarch27@users.noreply.github.com>
2024-09-02 17:11:39 +0700

    first commit

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import prisma from '@/lib/prisma'
+
+export default async function Blog({ params }: { params: { slug: string } }) {
+  const blog = await prisma.post.findUnique({
+    where: {
+      slug: params.slug
+    }
+  })
+  console.log('blog:', blog)
+
+  if (!blog) {
+    return <p>Blog not found</p>
+  }
+
+  return (
+    <section className='flex justify-center items-center h-scren'>
+      <div className='container'>
+        <h1 className='text-4xl font-bold'>{blog.title}</h1>
+        <div
+          className='prose-headings:font-title font-default prose mt-4 dark:prose-invert focus:outline-none'
+          dangerouslySetInnerHTML={{ __html: blog.content }}
+        ></div>
+      </div>
+    </section>
+  )
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,14 @@
+
+import ContentForm from '@/components/blog/content-form';
+import { Dashboard } from '@/components/blog/home';
+
+export default async function Blogs() {
+
+
+  return (
+    <section className="py-24">
+      {/* <ContentForm /> */}
+      <Dashboard />
+    </section>
+  );
+}
