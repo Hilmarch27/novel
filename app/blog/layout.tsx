50105405a47e8afbf50495d92c38442662c5f591
Hilman <117459721+Hilmarch27@users.noreply.github.com>
2024-09-03 15:29:48 +0700

    wip nav

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,11 +1,15 @@
+import Nav from "@/components/layout/nav";
+
 export default function DashboardLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section>
-      {children}
-    </section>
+    <div className="flex flex-col h-screen">
+      <Nav>
+        {children}
+        </Nav>
+    </div>
   );
 }
